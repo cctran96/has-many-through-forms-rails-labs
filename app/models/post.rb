8f1fe67c5269ee3e1e5a42cd00c725426dbe189c
Chau Tran <cctran96@gmail.com>
2021-06-14 10:15:17 -0700

    commit tree f318c864238cdebc14468342e59adec12b3777b2

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,7 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      if category_attribute[:name].present?
-        category = Category.find_or_create_by(category_attribute)
-        self.categories << category
-      end
+      self.categories << Category.find_or_create_by(category_attribute) unless category_attribute[:name].blank?
     end
   end
 end
